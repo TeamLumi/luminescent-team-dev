0d36f82786636965ab8cf741521fe5b9ed24158a
AarCon01 <bowtiesarecool110@gmail.com>
2026-02-20 12:53:03 -0700

    Fix the Relumi trainer list

--- src/components/Mapper/Mapper.jsx
+++ src/components/Mapper/Mapper.jsx
@@ -385,7 +385,7 @@ export const Mapper = ({ pokemonList3, pokemonList, pokemonListV }) => {
     setSelectedZone(location.name);
     setSelectedZoneId(location.zoneId)
     setEncounterList(setAllEncounters(location.zoneId));
-    setTrainerList(getTrainersFromZoneId(location.zoneId));
+    setTrainerList(getTrainersFromZoneId(location.zoneId, GAMEDATA3));
 
     setFieldItems(getFieldItemsFromZoneID(location.zoneId));
     setHiddenItems(getHiddenItemsFromZoneID(location.zoneId));
