208f13a2b2d408e6d3b385a719ffba098dc1da6e
AarCon01 <bowtiesarecool110@gmail.com>
2026-03-21 22:07:28 -0600

    Updates

--- assets/js/4fd21c3f.74d72e35.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkluminescent_team=globalThis.webpackChunkluminescent_team||[]).push([[18512],{647899(e){e.exports="/luminescent-team-dev/pokedex/ash-pikachu"}}]);
--- assets/js/9195e590.1d7e680e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkluminescent_team=globalThis.webpackChunkluminescent_team||[]).push([[961298],{543880(e){e.exports="/luminescent-team-dev/pokedex/ash-greninja"}}]);
